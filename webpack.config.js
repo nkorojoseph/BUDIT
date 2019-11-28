4a764ac38dc7d21b8cc71e28469a441bf6e4808a
Aiico <aiico@FLR2-HQ-CAP-24.Aiicoplc.com>
2019-11-28 09:42:54 +0100

    production env setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 //you must have entry-> output, __dirname
 const path = require('path')
-module.exports = {
-    entry:'./src/app.js',
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry:'./src/app.js',
     output:{
         //create the path to the local file we are working in
         path: path.join(__dirname,'public'),
@@ -21,9 +25,12 @@ module.exports = {
                 ]
         }]
     },
-    devtool:'cheap-module-eval-source-map',
+    devtool:isProduction?'source-map':'cheap-module-eval-source-map',
     devServer:{
         historyApiFallback: true,
         contentBase:path.join(__dirname,'public')
     }
+    }
 }
+
+
