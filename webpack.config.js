041d1ddc41508f9569906ab55d784689895ae155
Nkoro Joseph <nkorojoseph@yahoo.com>
2019-12-02 17:37:11 +0100

    dev tools separate

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,38 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = (env) => {
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css')
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    return {
-        entry:'./src/app.js',
-    output:{
-        //create the path to the local file we are working in
-        path: path.join(__dirname,'public'),
-        filename:'bundle.js'
+  return {
+    entry: './src/app.js',
+    output: {
+      //create the path to the local file we are working in
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
     },
-    module:{
-        rules: [{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,
-            use: CSSExtract.extract({
-                use: [
-                      'css-loader',
-                      'sass-loader'
-                    ]
-            })
-        }]
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: ['css-loader', 'sass-loader']
+          })
+        }
+      ]
     },
-    plugins: [
-        CSSExtract
-    ],
-    devtool:isProduction?'source-map':'inline-source-map',
-    devServer:{
-        historyApiFallback: true,
-        contentBase:path.join(__dirname,'public')
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, 'public'),
+      publicPath: '/dist/'
     }
-    }
-}
-
-
+  };
+};
