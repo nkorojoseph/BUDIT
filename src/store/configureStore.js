c97a538742cb178740c798c2fc92068cb2f64e8e
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2020-03-21 00:04:16 +0100

    private route for hiding some pages

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth'
+
 
 //create a const value from redux dev tools property else use compose.
 //if no dev tool, apply thunk directly without much config
@@ -10,7 +12,8 @@ export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
-      filters: filtersReducer
+      filters: filtersReducer,
+      auth: authReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
