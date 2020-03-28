543fe1bb6d846bb502ae0ab659a9c76965a71856
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2020-03-28 17:39:49 +0100

    user authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ export const Header = ({handleLogout}) => (
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     <button onClick={handleLogout}>Logout</button>
   </header>
 );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,19 +9,19 @@ import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 import createHistory from 'history/createBrowserHistory';
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory()
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
+       
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
