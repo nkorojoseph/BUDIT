ecc5ab9a599de1c3ccd63dc5e57541e55ebbce50
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2020-03-31 02:37:03 +0100

    styling components header and expensefilter component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {connect} from 'react-redux'
 import {handleLogout, startLogin} from '../actions/auth'
 
 export const Header = ({handleLogout}) => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <button onClick={handleLogout}>Logout</button>
+  <header className="header">
+    <div className="content-container">    
+      <div className="header__content">
+    <Link className="header__title" to="/dashboard" >
+      BUGIT
+    </Link>
+    <button onClick={handleLogout} className="button-layout button-layout--link">Logout</button>
+    </div>
+    </div>
   </header>
 );
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__out">
+      <h1 className="box-layout__title">BUGIT</h1>
+      <p>Manage your own expenses</p>
+    <button onClick={startLogin} className="button-layout">Login with Google</button>
+    </div>
   </div>
 );
 
